4a2c06b0fd001ccfa75fe7a41560972977674428
Kimiya Mohammadtaheri <63723127+kymmt80@users.noreply.github.com>
2023-05-05 23:12:57 +0330

    change app for provider id

--- src/App.js
+++ src/App.js
@@ -13,36 +13,40 @@ import User from './components/user';
 //import Provider from './components/provider';
 import Home from './components/home'
 import Provider from './components/provider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import axios from 'axios';
 import './css/error.css'
 import './css/login.css'
 import './css/user.css'
 import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+function ProviderHelp(){
+    let param=useParams();
+    return(<Provider param={param}/>)
+}
 
 function App() {
-    const toastStyle = {
-        fontFamily: 'Arial',
-        fontSize: '28px'
-    };
-    return (
-        <div>
-            <ToastContainer toastStyle={toastStyle}/>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<Home/>}/>
-                    <Route exact path="/commodity" element={<Commodity/>}/>
-                    <Route exact path="/user" element={<User/>}/>
-                    <Route exact path="/login" element={<Login/>}/>
-                    <Route exact path="/signup" element={<Signup/>}/>
-                    <Route exact path="/provider" element={<Provider/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
-
-// return (
+  const toastStyle = {
+    fontFamily: 'Arial',
+    fontSize: '28px'
+};
+  return (
+    <div>
+      <ToastContainer toastStyle={toastStyle}/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/commodity" element={<Commodity/>}/>
+          <Route exact path="/user" element={<User/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<Signup/>}/>
+          <Route exact path="/providers/:id" element={<ProviderHelp/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+  // return (
   //   <div className="App">
   //     <header className="App-header">
   //       <img src={logo} className="App-logo" alt="logo" />
