2ef286fa0e35c846b1bec1d926222b1b5df9fecb
Mahsa <92688832+mahsa-hemmat@users.noreply.github.com>
2023-05-05 23:30:37 +0330

    icon link added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,35 @@
 function Logo() {
     return (
-        <div class="logo">
-            <img src={require("../assets/logo.png")} />
-            <p href="#default" class="logo_name">Baloot</p>
+        <div className="logo">
+            <a href="/">
+                <img src={require("../assets/logo.png")} />
+            </a>
+            <p className="logo_name">Baloot</p>
         </div>
     )
 }
 
 function Home_Header(props) {
     return (
-        <div class="header container-fluid">
+        <div className="header container-fluid">
             <Logo />
-            <form class="header_search_bar">
-                <div class="dropdown">
-                    <button class="dropbtn dropdown-toggle" data-toggle="dropdown" type="button" /*onClick={props.handleDropdown}*/>{props.searchBy=="1"?"name":props.searchBy=="2"?"category":"provider"}</button>
-                    <div class="dropdown_content">
-                        <a href="#" class="dropdown-item" onClick={props.handleSearchByName}>name</a>
-                        <a href="#" class="dropdown-item" onClick={props.handleSearchByCategory}>category</a>
-                        <a href="#" class="dropdown-item" onClick={props.handleSearchByProvider}>provider</a>
+            <form className="header_search_bar">
+                <div className="dropdown">
+                    <button className="dropbtn dropdown-toggle" data-toggle="dropdown" type="button" /*onClick={props.handleDropdown}*/>{props.searchBy=="1"?"name":props.searchBy=="2"?"category":"provider"}</button>
+                    <div className="dropdown_content">
+                        <a href="#" className="dropdown-item" onClick={props.handleSearchByName}>name</a>
+                        <a href="#" className="dropdown-item" onClick={props.handleSearchByCategory}>category</a>
+                        <a href="#" className="dropdown-item" onClick={props.handleSearchByProvider}>provider</a>
                     </div>
                 </div>
                 <input type="text" onChange={props.handleSearchQuery} onKeyDown={props.handleSearch} placeholder="search your product ..." />
-                <div class="img_wrap"><img src="../../assets/search.png" /></div>
+                <div className="img_wrap"><img src="../../assets/search.png"/></div>
             </form>
-            <div class="header_right">
-            <p>#username</p>
-                <a class="brown_button" href="#contact">Cart 5</a>
+            <div className="header_right">
+                <a href="/user">
+                    <p>#username</p>
+                </a>
+                <a className="brown_button" href="/user">Cart 5</a>
             </div>
         </div>
     );
